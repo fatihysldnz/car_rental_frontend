668c54e10178955a2336ed1740deb27bbb0a1aa3
fatihysldnz <hfatih98@gmail.com>
2021-12-21 00:00:58 +0300

    v1

---
+++ src/pages/user/UserReservationDetailPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router";
+import PageHeader from "../../components/common/PageHeader";
+import Spacer from "../../components/common/Spacer";
+import Details from "../../components/user/Details";
+const UserReservationDetailPage = () => {
+  const { reservationId } = useParams(); // path de yazan reservation Id sine ulassin diye
+
+  return (
+    <>
+      <PageHeader title="Reservation Detail" />
+      <Spacer />
+      <Details reservationId={reservationId} />
+      <Spacer />
+    </>
+  );
+};
+export default UserReservationDetailPage;
